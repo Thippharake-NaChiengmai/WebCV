41ad09b93e7dfcae9788882cdc92708e8d2dda43
Thippharake-NaChiengmai <thipppharake.na@gmail.com>
2025-02-06 14:37:15 +0700

    final3

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+//fake-btn
 document.getElementById('fake-btn').addEventListener('click', function() {
     alert('This function is not available in practice. !!!');
 });
 
+//fade-in
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.body.classList.add('fade-in');
+  });
+
+  //showmore-showless
+const moreContent = document.getElementById('more-content');
+const toggleText = document.getElementById('toggle-text'); 
+const introContent = document.getElementById('intro-content');
+
+toggleText.addEventListener('click', () => { 
+    if (moreContent.style.display === 'none') {
+        moreContent.style.display = 'block';
+        toggleText.textContent = 'Show Less'; 
+    } else {
+        moreContent.style.display = 'none';
+        toggleText.textContent = 'Show More'; 
+    }
+});

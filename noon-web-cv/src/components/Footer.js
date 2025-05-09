148c3ab65ee4701a93bad31939e4e83e9aacda91
Thippharake-NaChiengmai <thipppharake.na@gmail.com>
2025-05-09 22:11:15 +0700

    Initial commit with Navbar and Footer components2

--- noon-web-cv/src/components/Footer.js
+++ noon-web-cv/src/components/Footer.js
@@ -4,7 +4,9 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
     return(
  <footer className="bg-dark text-white text-center py-5 px-5">
+    <div className="container">
         <p className="mb-0">&copy; {currentYear} Noon Web CV. All rights reserved.</p>
+        </div>
       </footer>
       );
 };
